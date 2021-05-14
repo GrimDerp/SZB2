86136f98746b2d1e68475fac05df70f3f3fe90c9
Brady Choate <admin@superzen.org>
2021-05-14 17:44:37 -0600

    corrrect running

--- Assets/Runner/Runner.cs
+++ Assets/Runner/Runner.cs
@@ -2,21 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+//stuff &* things
+
 public class Runner : MonoBehaviour
 {
 
     public static float distanceTraveled;
 
+    public Vector3 jumpVelocity;
+    public float m_Thrust = 20f;
+    
+
+    public float acceleration;
+
+    private bool touchingPlatform;
+
+    public Rigidbody rb;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
+
+   
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(5f * Time.deltaTime, 0f, 0f);
-        distanceTraveled = transform.localPosition.x;
+             distanceTraveled = transform.localPosition.x;
+
+             if(touchingPlatform && Input.GetButtonDown("Jump")) {
+                 rb.AddForce(jumpVelocity, ForceMode.VelocityChange);
+             }
     }
+
+
+
+    void FixedUpdate() {
+        if(touchingPlatform){
+            rb.AddForce(m_Thrust, 0f, 0f, ForceMode.Acceleration);
+        }
+
+          
+    }
+
+
+
+    void OnCollisionEnter() {
+        touchingPlatform = true;
+    }
+
+    void OnCollisionExit() {
+        touchingPlatform = false;
+    }
+
 }
